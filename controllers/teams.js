0f7c89b2b0dc3e55cbd39f67fa9be9bad3611826
ashco <ashchristie@gmail.com>
2017-12-28 17:35:15 -0800

    MVP achieved

--- controllers/teams.js
+++ controllers/teams.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
   var teams = teamService.allTeams();
-  res.render('teams/index', { teams: teams });
+  res.render('teams/index.ejs', { teams: teams });
 });
 
 router.post('/', function(req, res) {
@@ -15,14 +15,33 @@ router.post('/', function(req, res) {
 });
 
 router.get('/new', function(req, res) {
-  res.render('teams/new');
+  res.render('teams/new.ejs');
+});
+
+router.get('/edit/:name', function(req, res){
+  var team = teamService.getTeam(req.params.name);
+  res.render('teams/edit.ejs', { team: team });
 });
 
 router.get('/:name', function(req, res) {
   // search for the team name in all the teams.
   var team = teamService.getTeam(req.params.name);
 
-  res.render('teams/show', { team: team });
+  res.render('teams/show.ejs', { team: team });
 });
+
+router.put('/:name', function(req,res){
+  // console.log('name:', req.params.name);
+  // console.log('req.body:', req.body)
+  teamService.editTeam(req.params.name, req.body);
+  res.send('success');
+});
+
+router.delete('/:name', function(req,res){
+  console.log(req.params.name);
+  teamService.deleteTeam(req.params.name);
+  res.send('success');
+});
+
 
 module.exports = router;
--- public/js/teams-ajax.js
+++ public/js/teams-ajax.js
@@ -1,3 +1,23 @@
-$(document).ready(function() {
+$('.delete-link').click(function(e){
+  e.preventDefault()
+  $.ajax({
+    url: $(this).attr('href'),
+    method: 'DELETE',
+  }).done(function(data){
+    window.location.href = '/teams';
+  });
+});
 
+$('.edit-form').submit(function(e){
+  e.preventDefault();
+  $.ajax({
+    method: 'PUT',
+    url: $(this).attr('action'),
+    data: {
+      name: $('#new-name').val(),
+      members: $('#members').val()
+    }
+  }).done(function(response){
+    window.location.href = '/teams';
+  });
 });
